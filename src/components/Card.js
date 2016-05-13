ea65fd85cee33797b50c9f187eeede1b8a5870d8
Greg Gigon <greg.gigon@gmail.com>
2016-05-13 15:36:29 +0100

    Initial commit for the sample code

---
+++ src/components/Card.js
@@ -0,0 +1,104 @@
+import React, { Component, PropTypes } from 'react';
+import { findDOMNode } from 'react-dom';
+import { DragSource, DropTarget } from 'react-dnd';
+
+const style = {
+  border: '1px dashed gray',
+  padding: '0.5rem 1rem',
+  marginBottom: '.5rem',
+  backgroundColor: 'white',
+  cursor: 'move'
+};
+
+const cardSource = {
+  beginDrag(props) {
+    return {
+      id: props.id,
+      index: props.index
+    };
+  }
+};
+
+const cardTarget = {
+  hover(props, monitor, component) {
+    const dragIndex = monitor.getItem().index;
+    const hoverIndex = props.index;
+
+    // Don't replace items with themselves
+    if (dragIndex === hoverIndex) {
+      return;
+    }
+
+    // Determine rectangle on screen
+    const hoverBoundingRect = findDOMNode(component).getBoundingClientRect();
+
+    // Get vertical middle
+    const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+
+    // Determine mouse position
+    const clientOffset = monitor.getClientOffset();
+
+    // Get pixels to the top
+    const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+
+    // Only perform the move when the mouse has crossed half of the items height
+    // When dragging downwards, only move when the cursor is below 50%
+    // When dragging upwards, only move when the cursor is above 50%
+
+    // Dragging downwards
+    if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+      return;
+    }
+
+    // Dragging upwards
+    if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+      return;
+    }
+
+    // Time to actually perform the action
+    props.moveCard(dragIndex, hoverIndex);
+
+    // Note: we're mutating the monitor item here!
+    // Generally it's better to avoid mutations,
+    // but it's good here for the sake of performance
+    // to avoid expensive index searches.
+    monitor.getItem().setIndex(hoverIndex);
+  }
+};
+
+class Card extends Component {
+  render() {
+    const { text, isDragging, connectDragSource, connectDropTarget } = this.props;
+    const opacity = isDragging ? 0 : 1;
+    let styling = Object.assign(style, opacity);
+    return connectDragSource(connectDropTarget(
+      <div style={styling}>
+        {text}
+      </div>
+    ));
+  }
+}
+
+Card.propTypes = {
+  connectDragSource: PropTypes.func.isRequired,
+  connectDropTarget: PropTypes.func.isRequired,
+  index: PropTypes.number.isRequired,
+  isDragging: PropTypes.bool.isRequired,
+  id: PropTypes.any.isRequired,
+  text: PropTypes.string.isRequired,
+  moveCard: PropTypes.func.isRequired
+};
+
+const collectForDropTarget = connect => {
+  return {
+  connectDropTarget: connect.dropTarget()
+}};
+
+const collectForDragSource = (connect, monitor) => {
+  return {
+  connectDragSource: connect.dragSource(),
+  isDragging: monitor.isDragging()
+}};
+
+export default DragSource('card', cardSource, collectForDragSource)(
+  DropTarget('card', cardTarget ,collectForDropTarget)(Card));
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import 'core-js/fn/object/assign';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/Main';
+import Container from './components/Container';
+
+// Render the main component into the dom
+ReactDOM.render(<Container />, document.getElementById('app'));
